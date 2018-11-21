f0c95733a51ad41819cc05859a620666418604b1
Sandra <grahlsan@gmail.com>
2018-11-21 14:20:51 +0000

    package.json

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
+const http = require('http');
+const fs = require("fs");
+const path = require("path");
+const querystring = require('querystring');
 
+
+
+
+const server = http.createServer();
+
+server.listen(3000, function () {
+
+    console.log("Server is listening on port 3000.  Ready to accept requests!");
+});
