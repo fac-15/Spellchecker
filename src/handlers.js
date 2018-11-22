6f0dd6987f47632b7d88226655e9d1272314953a
wright1 <prosperwright@yahoo.co.uk>
2018-11-22 04:14:35 +0000

    serving website to browser

--- src/handlers.js
+++ src/handlers.js
@@ -1,15 +1,83 @@
-const handleHomeRoute = (request, resonse) => {
-  const filePath = path.join(__dirname, "..", url);
-  fs.readFile(filePath, (error, file) => {
-    if (error) {
-      resonse.writeHead(404, "Content-Type: text/html");
-      response.end("<h1>400, file not found</h1>");
-    } else {
-      response.writeHead(200, `Content-Type: ${extensionType[extension]}`);
+const fs = require('fs');
+const path = require('path');
+
+const handleHomeRoute = (request, response) => {
+
+  const filePath = path.join(__dirname, '..', 'public', 'index.html');
+  fs.readFile(filePath, (error, file) =>{
+
+    if(error){
+      console.log(error);
+      response.writeHead(500, {'Content-Type': 'text/html'});
+      response.end("<h1>Sorry, we're having problem at our end</h1>");
+    } else{
+      
+      response.writeHead(200, {'Content-Type': 'text/html'});
       response.end(file);
+
     }
   });
+
 };
+
+
+const handlePublic = (resquest, response, url) => {
+     
+     
+     const filePath = path.join(__dirname, '..', 'public', url);
+     const ext = url.split('.')[1];
+     const extType = {
+            html: "text/html",
+            css: "text/css",
+            js: "application/javascript",
+            ico: "image/x-icon"
+          };
+          
+    
+          fs.readFile(filePath, (error, file) => {
+
+            if(error){
+              console.log(error);
+              response.writeHead(200, {'Content-Type': 'text/html'});
+              response.end(404,"<h1>Sorry, we're having problem at our end</h1>");
+            }else{
+
+              response.writeHead(200, {'Content-Type': `${extType[ext]}`});
+              response.end(file);
+            }
+          })
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const handleHomeRoute = (request, resonse) => {
+//   const filePath = path.join(__dirname, "..", url);
+//   fs.readFile(filePath, (error, file) => {
+//     if (error) {
+//       resonse.writeHead(404, "Content-Type: text/html");
+//       response.end("<h1>400, file not found</h1>");
+//     } else {
+//       response.writeHead(200, `Content-Type: ${extensionType[extension]}`);
+//       response.end(file);
+//     }
+//   });
+// };
 
 //
 // const fs = require("fs");
@@ -27,7 +95,7 @@ const handleHomeRoute = (request, resonse) => {
 //
 //
 //
-// module.exports = {
-//     handleHomeRoute,
-//     handlePublic
-// };
+module.exports = {
+    handleHomeRoute,
+    handlePublic
+};
--- src/router.js
+++ src/router.js
@@ -1,45 +1,24 @@
-//works;
-const fs = require("fs");
+
+const handlers = require("./handlers.js");
 const path = require("path");
-// request.on("data", function() {
-//   // do something
-// });
-console.log("hi router");
+
+
 const router = (request, response) => {
-  console.log("hi inside router");
+  
+  console.log('I am inside the router')
+ 
   const url = request.url;
-  console.log(url);
-  if (url === "/") {
-    //home page
-
-    const filePath = path.join(__dirname, "..", "index.html");
-    console.log(filePath);
-    fs.readFile(filePath, (error, file) => {
-      if (error) {
-        console.log(error);
-        response.writeHead(500, "Content-Type: text/html");
-        response.end("<h1>Sorry, we have a problem at our end</h1>");
-      } else {
-        response.writeHead(200, "Content-Type: text/html");
-        response.end(file);
-      }
-    });
+  console.log('URL:', url)
+  if(url ==='/'){
+    handlers.handleHomeRoute(request, response);
+  }else if (url === '/style.css'){
+    console.log('I am css')
+    handlers.handlePublic(request, response, url);
   }
-  /////////////////
-  else if (url.indexOf("public") !== -1) {
-    const extension = url.split(".")[1];
+  
+}
 
-    const extensionType = {
-      html: "text",
-      css: "text/css",
-      js: "application/javascript",
-      ico: "public/image/"
-    };
-    response.writeHead(200, { "Content-Type": `${extensionType[extension]}` });
-    response.end(file);
-  } else {
-    response.writeHead(404);
-    response.end("<h1>404 not found</h1>");
-  }
-};
+
+
+
 module.exports = router;
