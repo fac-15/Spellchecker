903c0e492edd7668043facee0a2971694b550a1e
Lucy <lewright20@gmail.com>
2018-11-23 11:06:11 +0000

    backend filter added

--- public/request.js
+++ public/request.js
@@ -1,22 +1,10 @@
-const makeRequest = (inputValue) => {
+const makeRequest = (inputValue, callback) => {
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
       let obj = JSON.parse(xhr.responseText);
-      console.log('request', obj[0]); // successfully logging
-      let nameArr = obj.map((item) => {
-        if (item.name) {
-          return item.name;
-        } else {
-          return item.define;
-        }
-      });
-      let filteredNames = nameArr.filter((key) => {
-        if (key[0].indexOf(inputValue) !== -1) {
-          return key;
-        }
-      });
-      return filteredNames;
+      callback(obj);
+      //   console.log('request', obj[0]); // successfully logging
     }
   };
   xhr.open('GET', `/search?query=${inputValue}`, true);
