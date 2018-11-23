254350338679ea404c8db9de8588366beaf5dc18
Lucy <lewright20@gmail.com>
2018-11-23 14:23:10 +0000

    tweaking formatting dom

--- public/dom.js
+++ public/dom.js
@@ -8,22 +8,6 @@ const domCallback = (response, input) => {
     newItem.setAttribute('value', item.name);
     dropDown.appendChild(newItem);
   })
-  
-  
-  
-  
-  
-  
-  // for (let i = 0; i < input.length; i++) {
-
-  // }
-
-  // this.value.innerHTML = "<strong>" + dropDown[i].substr(0, val.length) + "</strong>";
-  // this.value.innerHTML += dropDown[i].substr(val.length);
-  // /*insert a input field that will hold the current array item's value:*/
-  // this.value.innerHTML += "<input type='hidden' value='" + dropDown[i] + "'>";
-  // // map from item to names/defines
-  // // use list of names for drop down
 };
 
 input.addEventListener('keyup', (e) => {
